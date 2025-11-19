05e622a3a084fb178bf7f701d5311fc77cd829ac
steven <scoaila@proton.me>
2025-11-19 18:11:34 -0500

    Initial commit: Vue.js tasks app setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TaskForm from "./components/TaskForm.vue";
+
+const message = ref("Hello World")
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+
+    <TaskForm ></TaskForm>
+  </main>
+</template>
+
+<style scoped>
+main {
+  color: red;
+  max-width: 800px;
+  margin: 1rem auto;
+}
+</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const newTask = ref("")
+
+function formSubmitted() {
+  console.log(newTask.value)
+}
+</script>
+
+<template>
+  <form @submit.prevent="formSubmitted">
+    <label>
+      New Task
+      <input v-model="newTask" type="text" name="newTask">
+    </label>
+
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+.button-container {
+  display: flex;
+  justify-content: end;
+}
+</style>
